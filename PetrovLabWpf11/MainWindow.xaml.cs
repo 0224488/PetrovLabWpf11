306a3c08c366638c0e281dbb0f5fe7badc83dda8
0224488 <0224488@inbox.ru>
2022-05-25 19:47:07 +0300

    Добавьте файлы проекта.

---
+++ PetrovLabWpf11/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+// Используйте Microsoft Visual Studio для создания геометрической формы подобно образцу. 
+// Для создания круга можно использовать два эллипса разного диаметра. 
+// Для создания перечеркивающей линии можно использовать прямоугольник, добавив к нему трансформацию – поворот.
+
+namespace PetrovLabWpf11
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
